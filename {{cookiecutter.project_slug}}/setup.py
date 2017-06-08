070e3092475ec3706c8e0e54ee90eaa157a2fa29
Davide Moro <dmoro@tierratelematics.com>
2017-06-08 10:04:27 +0200

    adjust indenting

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -25,6 +25,14 @@ docs_require = [
     'sphinx_rtd_theme',
     ]
 
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
 setup(name='{{cookiecutter.project_slug}}',
       version=version,
       description="{{ cookiecutter.project_short_description }}",
