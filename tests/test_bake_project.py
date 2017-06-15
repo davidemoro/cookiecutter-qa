b39a1f49758a327791db8c59aafb74369d1f9565
Davide Moro <dmoro@tierratelematics.com>
2017-06-15 17:34:41 +0200

    fix syntax error

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -134,8 +134,9 @@ def test_bake_without_testrail(cookies):
 def test_bake_and_run_tests(cookies):
     with bake_in_temp_dir(
             cookies,
-            extra_context={'browserstack_user': "davidemoro2",
-                           'browserstack_access_key': 'dXoGjyFSzem5hYywqYPx') as result:
+            extra_context={
+                'browserstack_user': "davidemoro2",
+                'browserstack_access_key': 'dXoGjyFSzem5hYywqYPx'}) as result:
         assert result.project.isdir()
         run_inside_dir('python setup.py test', str(result.project)) == 0
         print("test_bake_and_run_tests path", str(result.project))
