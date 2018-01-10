7b80abdde82cff521a1ba3f2b1e1aa3ab98e58f0
Davide Moro <dmoro@tierratelematics.com>
2018-01-10 11:40:48 +0100

    update pytest-play

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/functional/test_play.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/functional/test_play.py
@@ -1,9 +1,9 @@
 import os
 
 
-def test_play(play_json, data_getter):
-    data = data_getter(
-        os.path.abspath(os.path.dirname(__file__)),
+def test_play(play_json):
+    data = play_json.get_file_contents(
+        os.path.dirname(__file__),
         'data',
         'play.json')
     play_json.execute(data)
